3652a4aab57ace9127299e47c05f8354c82347d9
Almas <jobspace@yandex.ru>
2018-08-19 09:23:05 +0600

    init

---
+++ config/server.js
@@ -0,0 +1,67 @@
+// config used by server side only
+const dbHost = process.env.DB_HOST || 'ds117821.mlab.com';
+const dbPort = process.env.DB_PORT || 17821;
+const dbName = process.env.DB_NAME || 'golden';
+const dbUser = process.env.DB_USER || 'shopuser';
+const dbPass = process.env.DB_PASS || '31036111@shop';
+const dbCred =
+	dbUser.length > 0 || dbPass.length > 0 ? `${dbUser}:${dbPass}@` : '';
+
+const dbUrl =
+	process.env.DB_URL || `mongodb://${dbCred}${dbHost}:${dbPort}/${dbName}`;
+
+module.exports = {
+	// used by Store (server side)  mongodb://<dbuser>:<dbpassword>@ds117821.mlab.com:17821/golden
+	apiBaseUrl: `http://localhost:3001/api/v1`,
+
+	// used by Store (server and client side)
+	ajaxBaseUrl: `http://localhost:3001/ajax`,
+
+	// Access-Control-Allow-Origin
+	storeBaseUrl: `http://localhost:3000`,
+
+	// used by API
+	adminLoginUrl: '/admin/login',
+
+	apiListenPort: 3001,
+	storeListenPort: 3000,
+
+	// used by API
+	mongodbServerUrl: dbUrl,
+
+	smtpServer: {
+		host: '',
+		port: 0,
+		secure: true,
+		user: '',
+		pass: '',
+		fromName: '',
+		fromAddress: ''
+	},
+
+	// key to sign tokens
+	jwtSecretKey: '-adfaf989fa7sdf7g9797agadsPLKDJF8a',
+
+	// key to sign store cookies
+	cookieSecretKey: '-apjdfpKPJPlKJ9(&DfDFDFdkasjfw32',
+
+	// path to uploads
+	categoriesUploadPath: 'public/content/images/categories',
+	productsUploadPath: 'public/content/images/products',
+	filesUploadPath: 'public/content',
+	themeAssetsUploadPath: 'theme/assets/images',
+
+	// url to uploads
+	categoriesUploadUrl: '/images/categories',
+	productsUploadUrl: '/images/products',
+	filesUploadUrl: '',
+	themeAssetsUploadUrl: '/assets/images',
+
+	// store UI language
+	language: 'en',
+
+	// used by API
+	orderStartNumber: 1000,
+
+	developerMode: true
+};
